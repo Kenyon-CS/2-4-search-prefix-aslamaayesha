800e5833296bfe44faaf9a3b4f3d8ff659a1a535
aslamaayesha <157529926+aslamaayesha@users.noreply.github.com>
2024-02-05 01:23:47 -0500

    Update SearchPrefix.rb

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,10 @@
-def searchPrefix(arr,s)
-
+def search_items_starting_with(array, start_string)
+    result = array.select { |words| words.start_with?(start_string) }
+    result
 end
-
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
-
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+  
+  input1 = gets.chomp.split
+  puts "starts with:"
+  start1 = gets.chomp
+  output1 = search_items_starting_with(input1, start1)
+  puts "Output 1: #{output1}"
